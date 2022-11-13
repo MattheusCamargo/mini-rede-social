437487554bde67baf118cf4d3468c32938b29977
Mattheus Camargo <109574457+MattheusCamargo@users.noreply.github.com>
2022-11-13 18:55:54 -0300

    autenticação
    aqui só mexi no script de autenticação, entendi como funciona e como deve ser construído. A princípio eu não queria fazer consulta em banco de dados, mas pra ficar melhor e mais bem feito, vou adicionar o bd ao projeto.

--- script.php
+++ script.php
@@ -1,38 +1,22 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rede Social</title>
-</head>
-<body>
-    <div>
-        <?php
-            $email= "mattheus@gmail.com";
-            $senha= 123456;
-            $mail= false;
-            $pass= false;
-            $us= isset($_POST["campo1"])?$_POST["campo1"]:0;
-            if ($us == $email){
-                $mail= true;
-            }else {
-                $mail= false;
-            }
-            
-            $sen= isset($_POST["campo2"])?$_POST["campo2"]:0;
-            if ($sen == $senha){
-                $pass= true;
-            }else {
-                $pass= false;
-            }
+<?php
+    $usuario = $_POST["campo1"];
+    $senha = $_POST["campo2"];
 
-           if ($mail == true && $pass == true){
-            header("location: welcome.html");
-           } else {
-            header("location: login.html");
-           }
-        ?>
-    </div>
-</body>
-</html>
+    $query = "SELECT * FROM usuarios WHERE usuario='$usuario' and senha='$senha'";
+    $conexao = new PDO('mysql:host=127.0.0.1;dbname=sitepessoal', 'root', ' ');
+    $resultado = $conexao->query($query);
+    $logado = $resultado->fetch();
+    $id_logado = $logado['id'];
+
+    if ($logado == null) {
+        // Usuário ou senha inválida
+        header('Location: usuario-erro.php');
+     } 
+     else {
+        session_start();
+        $_SESSION['usuario_logado'] = $id_logado;
+        // Direciona o usuário para o painel administrativo do sistema
+        header('Location: welcome.html');
+     }
+     die();
+?>
