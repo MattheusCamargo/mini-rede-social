a1836b89b059e1de7b99214ed940d849fe37a872
Mattheus Camargo <109574457+MattheusCamargo@users.noreply.github.com>
2022-11-14 00:37:33 -0300

    ajustando o insert

--- scriptcadas.php
+++ scriptcadas.php
@@ -4,7 +4,9 @@
     $sexo= NULL;
     $pass= $_POST['senha'];
 
-    $query= "INSERT INTO usuarios VALUES ('$nome', '$email', '$sexo', '$pass');";
-    $conexao = new PDO('mysql:host=127.0.0.1:3306;dbname=sitepessoal', 'root', '');
+    $query= "INSERT INTO usuarios (nome, email, sexo, senha) VALUES ('{$nome}', '{$email}', '{$sexo}', '{$pass}')";
+    $conexao= msqli_connect('127.0.0.1:3306', 'root', '', 'sitepessoal');
+    mysqli_query($conexao, $query);
+    mysqli_close($conexao);
     header('Location: login.html');
 ?>
